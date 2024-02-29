615146b61fd294bb1eeb23bc40e85e89a89c5890
MirsaidovUmed <umedmirsaidov61@gmail.com>
2024-02-29 13:12:28 +0500

    Added output using templates

--- index.php
+++ index.php
@@ -11,8 +11,26 @@ use App\Interfaces\IRender;
 
 require_once 'autoload.php';
 
-$warAndPeace = new TelegraphText('Leo Tolstoy', 'war-and-peace', 'Leo', 'Slug');
+$warAndPeace = new TelegraphText('Leo Tolstoy', 'war-and-peace');
 $warAndPeace->editText('War and Peace', 'Eh bien, mon prince. Gênes et Lucques...');
 
 $storage = new FileStorage();
 $storage->create($warAndPeace);
+
+$swig = new Swig('telegraph_text');
+$swig->addVariablesToTemplate(['slug', 'text']);
+
+$txt = new Txt('telegraph_text');
+$txt->addVariablesToTemplate(['slug', 'title', 'text', 'author']);
+
+$spl = new Spl('telegraph_text');
+$spl->addVariablesToTemplate(['slug', 'title', 'text']);
+
+$templateEngines = [$swig, $txt, $spl];
+foreach ($templateEngines as $engine) {
+    if ($engine instanceof IRender) {
+        echo $engine->render($warAndPeace) . PHP_EOL;
+    } else {
+        echo 'Template engine does mot support render interface' . PHP_EOL;
+    }
+}
